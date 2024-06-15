2609a9d0f04dbf091687db9ff03ea3f72088db76
Joshua Shaffer <joshua@joshuatshaffer.com>
2024-06-15 15:39:04 -0500

    Got a random redirect working.

--- src/random_redirect.nim
+++ src/random_redirect.nim
@@ -1,15 +1,31 @@
 import prologue
 import karax/[karaxdsl, vdom]
+import random
+
+const links = [
+  "/example/one",
+  "/example/two",
+  "/example/three",
+]
 
 proc hello*(ctx: Context) {.async.} =
   let x = buildHtml(html(lang = "en")):
     head:
-      meta(charset = "UTF-8", name="viewport", content="width=device-width, initial-scale=1")
+      meta(charset = "UTF-8", name = "viewport",
+          content = "width=device-width, initial-scale=1")
       title: text "Simple Chat"
     body:
       h1: text "Hello, World!"
+      a(href = "/random"): text "Random"
   resp htmlResponse("<!DOCTYPE html>\n" & $x)
+
+proc randomRedirect*(ctx: Context) {.async.} =
+  resp redirect(links[rand(links.high)])
+
+# Initialize the random number generator
+randomize()
 
 let app = newApp()
 app.get("/", hello)
+app.get("/random", randomRedirect)
 app.run()
